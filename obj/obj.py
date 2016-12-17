f623e3f0398cd8b294228a920c4509477c717ead
xubuntu <xubuntu@xubuntu-VirtualBox.(none)>
2016-12-17 07:20:02 -0500

    dodane pola i obwody

--- obj/obj.py
+++ obj/obj.py
@@ -13,7 +13,28 @@ class Trojkat(Figura):
 		self.angles = 3
 
 class Prostokat(Czworokat):
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+	def pole(self):
+		return a*b
+	def obwod(self):
+		return 2*a+2*b
 
 class Kwadrad(Prostokat):
+	def __init__(self, a):
+		self.a = a
+	def pole(self):
+		return a*a
+	def obwod(self):
+		return 4*a
 
 class Trapez(Czworokat):
+	def __init__(self, a, b, c, h):
+		self.a = a
+		self.b = b
+		self.h = h
+	def pole(self):
+		return (a+b)/2*h
+	def obwod(self):
+		return a+b+2*c
