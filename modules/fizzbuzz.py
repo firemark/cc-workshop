4f59f2f96d2978900ae07ce97897f12509bb2ec6
patyk.47 <patrycja.tobola@gmail.com>
2016-12-17 06:04:10 -0500

    better version

--- modules/fizzbuzz.py
+++ modules/fizzbuzz.py
@@ -1,19 +1,22 @@
-def fizzbuzz(n,fizz='fizz',buzz='buzz'):
+def fizzbuzz(n,f='fizz',b='buzz'):
     x=''
     for i in range(1,n+1):
-        if (i%3==0 and i%5!=0):
-            x+=fizz
-            x+='\n'
-        elif (i%5==0 and i%3!=0):
-            x+=buzz
-            x+='\n'
-        elif (i%5==0 and i%3==0):
-            x+=fizz
-            x+=buzz
-            x+='\n'
+        if (i%5==0 and i%3==0):
+            x+=f+b
+        elif i%3==0:
+            x+=f
+        elif i%5==0:
+            x+=b
         else:
             x+=str(i)
-            x+='\n'
+        x+='\n'
     return x
+
+def fizzbuzz_single(i,f,b):
+    if (i%5==0 and i%3==0): return f+b
+    elif i%3==0: return f
+    elif i%5==0: return b
+    else: return str(i)
+
 
 print fizzbuzz(20,'ziemniak','kartofel')
